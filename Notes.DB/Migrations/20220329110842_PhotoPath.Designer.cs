33b41d69cfc452f8e791e39b3a6fd99c8789acdc
Robert5435 <robert.nitza5@gmail.com>
2022-03-29 15:21:29 +0300

    Migrations PhotoPath

--- Notes.DB/Migrations/20220329110842_PhotoPath.Designer.cs
+++ Notes.DB/Migrations/20220329110842_PhotoPath.Designer.cs
@@ -4,14 +4,16 @@ using BeMyTeacher.DB;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BeMyTeacher.DB.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220329110842_PhotoPath")]
+    partial class PhotoPath
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
